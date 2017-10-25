2fb17eedcad4836947f2460d5e41257f812448db
Adrian <adrian.grzebyk@gmai.com>
2017-10-25 23:09:32 +0200

    participant service

--- server/src/main/java/pl/quayal/app/controller/ParticipantController.java
+++ server/src/main/java/pl/quayal/app/controller/ParticipantController.java
@@ -6,11 +6,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.quayal.app.model.Participant;
+import pl.quayal.app.service.ParticipantService;
 
 @RestController
 @RequestMapping("/participants")
 public class ParticipantController {
 
-    @GetMapping("/{participantsId}")
-    public Participant getParticipant(@PathVariable("participantsId") Long participantsId){}
+    private ParticipantService participantService;
+
+    public ParticipantController (ParticipantService participantService){
+        this.participantService = participantService;
+    }
+
+    @GetMapping("/{participantId}")
+    public Participant getParticipant(@PathVariable("participantId") Long participantId){
+        return participantService.getParticipant(participantId);
+        }
+
 }
--- server/src/main/java/pl/quayal/app/service/ParticipantService.java
+++ server/src/main/java/pl/quayal/app/service/ParticipantService.java
@@ -1,4 +1,18 @@
 package pl.quayal.app.service;
 
+import org.springframework.stereotype.Service;
+import pl.quayal.app.model.Participant;
+import pl.quayal.app.repository.ParticipantRepository;
+
+@Service
 public class ParticipantService {
+
+    private ParticipantRepository participantRepository;
+
+    public ParticipantService(ParticipantRepository participantRepository) {
+        this.participantRepository = participantRepository;
+    }
+    public Participant getParticipant(Long participantId){
+        return participantRepository.findOne(participantId);
+    }
 }
